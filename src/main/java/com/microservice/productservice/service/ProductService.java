72c66e4281d532dcefbe52e15d71a8040fb41a19
Paul <paulsingh4118@gmail.com>
2024-01-03 22:11:17 -0500

    Init - Controller, Model, Repo, Service

---
+++ src/main/java/com/microservice/productservice/service/ProductService.java
@@ -0,0 +1,29 @@
+package com.microservice.productservice.service;
+
+import com.microservice.productservice.dto.ProductRequest;
+import com.microservice.productservice.model.Product;
+import com.microservice.productservice.repository.ProductRepository;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+        productRepository.save(product);
+        log.info("Product {}", product.getId());
+    }
+
+}
