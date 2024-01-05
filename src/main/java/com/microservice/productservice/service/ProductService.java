ec3f6b38fd4f10243698d968c4105abf0d4ec661
Paul <paulsingh4118@gmail.com>
2024-01-04 23:33:14 -0500

    Wrote Integration Tests for Product Controller

--- src/main/java/com/microservice/productservice/service/ProductService.java
+++ src/main/java/com/microservice/productservice/service/ProductService.java
@@ -1,12 +1,14 @@
 package com.microservice.productservice.service;
 
 import com.microservice.productservice.dto.ProductRequest;
+import com.microservice.productservice.dto.ProductResponse;
 import com.microservice.productservice.model.Product;
 import com.microservice.productservice.repository.ProductRepository;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 
 
 @Service
@@ -24,6 +26,21 @@ public class ProductService {
                 .build();
         productRepository.save(product);
         log.info("Product {}", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts(){
+        List<Product> p = productRepository.findAll();
+       return p.stream().map(this::mapToProductResponse).toList();
+
+    }
+
+    private ProductResponse mapToProductResponse(Product p){
+        return ProductResponse.builder()
+                .id(p.getId())
+                .name(p.getName())
+                .description((p.getDescription()))
+                .price(p.getPrice())
+                .build();
     }
 
 }
